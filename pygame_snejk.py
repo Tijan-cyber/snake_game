74e621be30d500d2aaaac9d647b9e7c7aa0a8174
Tijan Košir <tijan.kosir@gmail.com>
2026-02-05 20:13:09 +0100

    konec 5.2.2026

--- pygame_snejk.py
+++ pygame_snejk.py
@@ -1,4 +1,175 @@
+#Napiši snake-game v pygamu
+#koda od prej ti lahko sliži za inspiracijo (npr dolzina kace s kvadratki -> to pride prav)
+
+#za projekt naredite github repo, in spremembe sproti comitatje addajte in pushajte
+#na koncu morajo biti v repozitoriju vsaj 3 vecji commiti
+
+#1. dodatna naloga:
+#naredi branch "izgled"
+#v tem brancu naredi logiko, da ko igra tece, lahko pritisnes gumb "space" kar celotni kaci nastavi nakljucno barvo
+
+#2. dodatna naloga:
+#naredi branch "multiplayer"
+#v tem branchu naredi logiko, da sta na zacetku igre 2 kaci, ena se upravlja z wasd, druga z gumbi s puscicami
+#ce se aca zabije vase, v drugo kaco ali v steno, izgubi
+
+#3. dodatna naloga
+#naredi megre obeh branchov 
+
+
+
 import pygame
+import copy
+import time
+import random
+
+class Kaca:
+	def __init__(self):
+		self.smer = "gor"
+		self.zivljenje = True
+		self.pozicije = [[1000, 600], [1010, 600]]
+
+
+
+	def sprememba_smeri(self, pritisk):
+		smeri = ["gor", "levo", "dol", "desno"]
+		if pritisk == "a":
+			self.smer = "levo"
+		elif pritisk == "d":
+			self.smer = "desno"
+		elif pritisk == "w":
+			self.smer = "gor"
+		elif pritisk == "s":
+			self.smer = "dol"
+
+		print(self.smer)
+
+		
+
+	def premik(self, podaljsevanje=False):
+		potem = copy.deepcopy(self.pozicije)
+
+		if self.smer == "gor":
+				potem[0][0] -= 1
+
+		elif self.smer == "dol":
+				potem[0][0] += 1
+
+
+		elif self.smer == "levo":
+				potem[0][1] -= 1
+
+		elif self.smer == "desno":
+				potem[0][1] += 1
+
+		for i in range(1, len(potem)):
+			potem[i] = self.pozicije[i-1]
+		if podaljsevanje:
+			potem.append(self.pozicije[-1]) 
+
+
+
+		
+
+		self.pozicije = potem
+
+
+
+
+
+class Polje(Kaca):
+	def __init__(self):
+		super().__init__()
+		self.rezultat = 0
+		self.hrana = [350,450]
+		self.znotraj = True
+		self.igra = True
+
+
+
+
+
+	def znotraj_check(self):
+		if self.pozicije[0][0] < 10 or self.pozicije[0][1] < 10 or self.pozicije[0][0] > 1990 or self.pozicije[0][1] > 1190:  #PAZI SIRINE RECTOV!!!
+			self.znotraj = False
+			print("Šu si vn")
+		for x in self.pozicije:
+			if self.pozicije.count(x) > 1:
+				self.znotraj = False
+				print("Pojedu si se")
+				break
+
+
+
+	def nova_hrana(self):
+		snake = self.pozicije
+
+		possible = [
+			[x, y]
+			for x in range(10, 2000)
+			for y in range(10, 1200)
+			if [x, y] not in snake
+		]
+
+		self.hrana = random.choice(possible)
+
+
+
+	def igranje(self):
+		while self.znotraj and self.igra:
+			clock.tick(120)
+
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					self.igra = False
+
+
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_w:
+						Kaca.sprememba_smeri("w")
+
+					if event.key == pygame.K_s:
+						Kaca.sprememba_smeri("s")
+
+					if event.key == pygame.K_a:
+						Kaca.sprememba_smeri("a")
+
+					if event.key == pygame.K_d:
+						Kaca.sprememba_smeri("d")
+
+			screen.fill(bg)
+
+			pygame.draw.circle(screen, (0, 255, 0), (self.hrana[0], self.hrana[1]), 10)
+			if self.pozicije[0] == self.hrana:
+				self.premik(podaljsevanje=True)
+				self.nova_hrana()
+			else:
+				self.premik()
+			for i in self.pozicije:
+				x = i[0]
+				y = i[1]
+				try: 
+					if self.pozicije.index(i) == 0:
+						pygame.draw.circle(screen, (0,225,255), (x,y+10), 10)
+					else:
+						pygame.draw.rect(screen, (0,225,255), (x,y,20,20))
+				except Exception as e:
+					self.znotraj = False
+
+			self.znotraj_check()
+
+			
+			pygame.display.flip()
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -11,12 +182,15 @@ igra = True
 
 clock = pygame.time.Clock()
 
-while igra:
-	clock.tick(1000)
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			igra = False
+
+igralna_plosca = Polje()
+kaca = Kaca()
 
 
-	screen.fill(bg)
-	pygame.display.update()
+igralna_plosca.igranje()
+
+
+
+
+
+
